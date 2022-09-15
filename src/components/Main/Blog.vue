6873d77234b54793a8ed7178ff30083b9240a012
otabekabdusalomov <otabekabdusalamov2001@gmail.com>
2022-09-15 13:41:50 +0300

    blog-section

--- src/components/Main/Blog.vue
+++ src/components/Main/Blog.vue
@@ -1,23 +1,23 @@
 <template>
     <section class="blog">
-        <div class="container blog-container d-flex align-items-center justify-content-between">
-            <img class="blog-right-img" src="../../assets/imgs/blog-img.png" alt="blog-img" width="250" height="250">
-            <div class="blog-left">
-               <div class="blog-left-mein">
-                <h2 class="blog-heading m-0 p-0 pb-18">Subscribe To Our Blog</h2>
-                <p class="blog-desc m-0 p-0">A newsletter is a tool used to communicate regularly with
+        <div class="container blog-container">
+            <img class="blog-left-img" src="../../assets/imgs/blog-img.png" alt="img" width="250" height="250">
+            <div class="blog-box-right">
+                <h2 class="blog-box-right-heading">Subscribe To Our Blog</h2>
+                <p class="blog-box-right-desc">A newsletter is a tool used to communicate regularly with
                     your subscribers, delivering the information</p>
-
-                 <div class="blog-left-engin">
-                    <img src="../../assets/imgs/Mail.svg" alt="img" width="19" height="19">
                     
-                 </div>   
-               </div>
-                    
-                    
-                </div>
-              
+               <div class="blog-box-three">
+                  <img class="blog-box-three-img" src="../../assets/imgs/Mail.svg" alt="img" width="19" height="19">
+                  <input class="blog-box-three-input" type="text" placeholder="Type Your Email Address">
+                  
+                  <button class="site-btn hero-btn">Get started
+                    <img class="design-button-img" src="../../assets/imgs/design-line.png" alt="design-line-img" width="25" height="25">
+                </button>
+               </div>     
             </div>
+            </div>
+            
         </section>
     </template>
     
@@ -30,24 +30,26 @@
     
     
     <style>
-        
         .blog {
-            padding-bottom: 160px;
+            padding-bottom: 161px;
         }
-        .blog-left-mein {
-            max-width: 632px;
+        .blog-container {
+            display: flex;
+            align-items: center;
         }
-        .blog-heading {
+        .blog-box-right-heading {
+            margin: 0;
             font-weight: 700;
             font-size: 52px;
             line-height: 72px;
             color: #56597A;
-        }    
-        .blog-desc{
+        }
+        .blog-box-right-desc {
+            margin: 0;
             font-weight: 400;
             font-size: 20px;
             line-height: 32px;
-            text-align: center;
             color: #919299;
+            padding: 0;
         }
     </style>
